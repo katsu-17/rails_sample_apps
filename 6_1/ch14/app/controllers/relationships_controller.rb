273494395ac129ae0e8948b773a75e50d16f1c2e
katsu-17 <tsutsui.katsushi@gmail.com>
2024-03-30 10:36:42 +0900

    指摘修正

--- 6_1/ch14/app/controllers/relationships_controller.rb
+++ 6_1/ch14/app/controllers/relationships_controller.rb
@@ -4,7 +4,7 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
-    create_or_update_notification
+    @user.upsert_notification(current_user)
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
@@ -18,37 +18,5 @@ class RelationshipsController < ApplicationController
       format.html { redirect_to @user }
       format.js
     end
-  end
-
-  private
-
-  def create_or_update_notification
-    return if already_created?
-
-    last_notification = Notification.where(user_id: @user.id).order(updated_at: :desc).first
-    if last_notification.present? && Time.current <= last_notification.updated_at + 5.minutes
-      update_notification(last_notification)
-    else
-      create_notification
-    end
-  end
-
-  def already_created?
-    Notification.where(user_id: @user.id).where("#{current_user.id} = ANY (follower_ids)").present?
-  end
-
-  def update_notification(last_notification)
-    follower_ids = last_notification.follower_ids
-    follower_ids.push(current_user.id)
-    last_notification.update(notification_type: Notification.notification_types[:multiple_follows],
-                             follower_ids: follower_ids)
-  end
-
-  def create_notification
-    notification = Notification.new(user_id: @user.id,
-                                    notification_type: Notification.notification_types[:follow],
-                                    follower_ids: [current_user.id],
-                                    follower_name: current_user.name)
-    notification.save
   end
 end
